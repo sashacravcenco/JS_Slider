321cbbfc3fb0925d69745e9db6f5fe56dd938c03
splincode <omaxphp@yandex.com>
2020-11-16 16:43:47 +0200

    added js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ var pauseButton = document.getElementById('pause');
 var next = document.getElementById('next');
 var previous = document.getElementById('prev');
 
- 
+let controls = document.querySelectorAll('.controls');
+
+for (let i = 0; i < controls.length; i++){
+controls[i].style.display = 'inline-block';
+} 
 
 function goToSlide(n) {
     slides[currentSlide].className = 'slide';
@@ -32,6 +36,24 @@ function playSlideshow() {
     slideInterval = setInterval(nextSlide,2000);
 }
 
+document.onkeydown = function(e) {
+    event.preventDefault();
+
+    switch (e.keyCode) {
+        case 37:
+            previousSlide();
+            break;
+        case 39:
+            nextSlide();
+            break;
+        case 32:
+            if (playing)
+            pauseSlideshow(); 
+            else playSlideshow();
+            break;
+    }
+};
+
 next.onclick = function() {
     pauseSlideshow();
     nextSlide();
@@ -49,5 +71,5 @@ pauseButton.onclick = function() {
   }
 }
 
-nextSlide()
+
 
